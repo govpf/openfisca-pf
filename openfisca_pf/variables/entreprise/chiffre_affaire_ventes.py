65a2494295c2f830bc9d831b913ba3e8eb48cbf6
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2020-07-31 12:21:21 -1000

    ventes fields for IT finished

--- openfisca_pf/variables/entreprise/chiffre_affaire_ventes.py
+++ openfisca_pf/variables/entreprise/chiffre_affaire_ventes.py
@@ -39,13 +39,157 @@ class chiffre_affaire_bagettes_revente_au_detail(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie revente au étail"
+    label = "Chiffre d'affaire dans la catégorie revente au détail"
 
 class charges_bagettes_revente_au_detail(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie revente au étail"
+    label = "Charges dans la catégorie revente au détail"
+
+class chiffre_affaire_coprah(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie coprah"
+
+class charges_coprah(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie coprah"
+
+class chiffre_affaire_farine_riz_sucre(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie farine, riz, sucre"
+
+class charges_farine_riz_sucre(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie farine, riz, sucre"
+
+class chiffre_affaire_hydrocarbures_au_detail(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie hydrcarbures au détail"
+
+class charges_hydrocarbures_au_detail(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie hydrcarbures au détail"
+
+class chiffre_affaire_importateurs_grossistes(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie importateurs grossistes"
+
+class charges_importateurs_grossistes(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie importateurs grossistes"
+
+class chiffre_affaire_lait_frais(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie lait frais"
+
+class charges_lait_frais(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie lait frais"
+
+class chiffre_affaire_negociants_detaillants_ca_sup_20_millions(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie négociants détaillants CA > 20M"
+
+class charges_negociants_detaillants_ca_sup_20_millions(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie négociants détaillants CA > 20M"
+
+class chiffre_affaire_tabacs(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie tabacs"
+
+class charges_tabacs(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie tabacs"
+
+class chiffre_affaire_timbres_postes_et_fiscaux(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie timbres postes et fiscaux"
+
+class charges_timbres_postes_et_fiscaux(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie timbres postes et fiscaux"
+
+class chiffre_affaire_ventes_a_l_aventure_armateurs(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie ventes à l'aventure (armateurs)"
+
+class charges_ventes_a_l_aventure_armateurs(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie ventes à l'aventure (armateurs)"
+
+class chiffre_affaire_ventes_a_l_aventure_goelettes(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie ventes à l'aventure (goelettes)"
+
+class charges_ventes_a_l_aventure_goelettes(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie ventes à l'aventure (goelettes)"
+
+class chiffre_affaire_ventes_en_gros(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie ventes en gros"
+
+class charges_ventes_en_gros(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie ventes en gros"
+
+class chiffre_affaire_ventes_en_gros_lait_frais_origine_local(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie ventes en gros lait frais d'origine local"
+
+class charges_ventes_en_gros_lait_frais_origine_local(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie ventes en gros lait frais d'origine local"
 
 class chiffre_affaire_ventes_sans_abattement(Variable):
     value_type = float
