f85f10a6bd09c639fff5cb14c522197b07be4072
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2020-08-05 21:14:58 -1000

    moved chiffre_affaire_total_ventes and added some totals

--- openfisca_pf/variables/entreprise/chiffre_affaire_ventes.py
+++ openfisca_pf/variables/entreprise/chiffre_affaire_ventes.py
@@ -9,20 +9,6 @@ from openfisca_core.model_api import *
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_pf.entities import *
 
-class chiffre_affaire_total_ventes(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = u"Montant total du chiffre d'affaire concernant des ventes avant abattement"
-    reference = "https://law.gov.example/income_tax"  # Always use the most official source
-
-    # The formula to compute the income tax for a given person at a given period
-    def formula(entreprise, period, parameters):
-        value = 0
-        for nom in [*parameters(period).dicp.it.abattements_it.ventes]:
-            value += entreprise('chiffre_affaire_' + nom, period)
-        return value
-
 class chiffre_affaire_apport_en_societe(Variable):
     value_type = float
     entity = Entreprise
