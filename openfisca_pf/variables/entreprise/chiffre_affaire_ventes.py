7f9f269364b5aeab06ee0d86ab88cd9022410860
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2020-08-06 18:35:56 -1000

    added seuil abattement and cca

--- openfisca_pf/variables/entreprise/chiffre_affaire_ventes.py
+++ openfisca_pf/variables/entreprise/chiffre_affaire_ventes.py
@@ -9,194 +9,196 @@ from openfisca_core.model_api import *
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_pf.entities import *
 
+# This file was generated with an automated tool
+
 class chiffre_affaire_apport_en_societe(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie apport en société"
+    label = "Chiffre d'affaire dans la catégorie Apport en société"
 
 class charges_apport_en_societe(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie apport en société"
+    label = "Charges dans la catégorie Apport en société"
 
 class chiffre_affaire_bagettes_revente_au_detail(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie revente au détail"
+    label = "Chiffre d'affaire dans la catégorie Bagettes revente au détail"
 
 class charges_bagettes_revente_au_detail(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie revente au détail"
+    label = "Charges dans la catégorie Bagettes revente au détail"
 
 class chiffre_affaire_coprah(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie coprah"
+    label = "Chiffre d'affaire dans la catégorie Coprah"
 
 class charges_coprah(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie coprah"
+    label = "Charges dans la catégorie Coprah"
 
 class chiffre_affaire_farine_riz_sucre(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie farine, riz, sucre"
+    label = "Chiffre d'affaire dans la catégorie Farine, riz, sucre"
 
 class charges_farine_riz_sucre(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie farine, riz, sucre"
+    label = "Charges dans la catégorie Farine, riz, sucre"
 
 class chiffre_affaire_hydrocarbures_au_detail(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie hydrcarbures au détail"
+    label = "Chiffre d'affaire dans la catégorie Hydrcarbures au détail"
 
 class charges_hydrocarbures_au_detail(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie hydrcarbures au détail"
+    label = "Charges dans la catégorie Hydrcarbures au détail"
 
 class chiffre_affaire_importateurs_grossistes(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie importateurs grossistes"
+    label = "Chiffre d'affaire dans la catégorie Importateurs grossistes"
 
 class charges_importateurs_grossistes(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie importateurs grossistes"
+    label = "Charges dans la catégorie Importateurs grossistes"
 
 class chiffre_affaire_lait_frais(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie lait frais"
+    label = "Chiffre d'affaire dans la catégorie Lait frais"
 
 class charges_lait_frais(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie lait frais"
+    label = "Charges dans la catégorie Lait frais"
 
 class chiffre_affaire_negociants_detaillants_ca_sup_20_millions(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie négociants détaillants CA > 20M"
+    label = "Chiffre d'affaire dans la catégorie Négociants détaillants CA > 20M"
 
 class charges_negociants_detaillants_ca_sup_20_millions(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie négociants détaillants CA > 20M"
+    label = "Charges dans la catégorie Négociants détaillants CA > 20M"
 
 class chiffre_affaire_tabacs(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie tabacs"
+    label = "Chiffre d'affaire dans la catégorie Tabacs"
 
 class charges_tabacs(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie tabacs"
+    label = "Charges dans la catégorie Tabacs"
 
 class chiffre_affaire_timbres_postes_et_fiscaux(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie timbres postes et fiscaux"
+    label = "Chiffre d'affaire dans la catégorie Timbres postes et fiscaux"
 
 class charges_timbres_postes_et_fiscaux(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie timbres postes et fiscaux"
-
-class chiffre_affaire_ventes_a_l_aventure_armateurs(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie ventes à l'aventure (armateurs)"
-
-class charges_ventes_a_l_aventure_armateurs(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Charges dans la catégorie ventes à l'aventure (armateurs)"
-
-class chiffre_affaire_ventes_a_l_aventure_goelettes(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie ventes à l'aventure (goelettes)"
-
-class charges_ventes_a_l_aventure_goelettes(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Charges dans la catégorie ventes à l'aventure (goelettes)"
-
-class chiffre_affaire_ventes_en_gros(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie ventes en gros"
-
-class charges_ventes_en_gros(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Charges dans la catégorie ventes en gros"
-
-class chiffre_affaire_ventes_en_gros_lait_frais_origine_local(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie ventes en gros lait frais d'origine local"
-
-class charges_ventes_en_gros_lait_frais_origine_local(Variable):
-    value_type = float
-    entity = Entreprise
-    definition_period = YEAR
-    label = "Charges dans la catégorie ventes en gros lait frais d'origine local"
+    label = "Charges dans la catégorie Timbres postes et fiscaux"
 
 class chiffre_affaire_ventes_sans_abattement(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie ventes sans abattement"
+    label = "Chiffre d'affaire dans la catégorie Ventes sans abattement"
 
 class charges_ventes_sans_abattement(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie ventes sans abattement"
+    label = "Charges dans la catégorie Ventes sans abattement"
+
+class chiffre_affaire_ventes_a_l_aventure_armateurs(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie Ventes à l'aventure (armateurs)"
+
+class charges_ventes_a_l_aventure_armateurs(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie Ventes à l'aventure (armateurs)"
+
+class chiffre_affaire_ventes_a_l_aventure_goelettes(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie Ventes à l'aventure (goelettes)"
+
+class charges_ventes_a_l_aventure_goelettes(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie Ventes à l'aventure (goelettes)"
+
+class chiffre_affaire_ventes_en_gros(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie Ventes en gros"
+
+class charges_ventes_en_gros(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie Ventes en gros"
+
+class chiffre_affaire_ventes_en_gros_lait_frais_origine_local(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Chiffre d'affaire dans la catégorie Ventes en gros lait frais d'origine local"
+
+class charges_ventes_en_gros_lait_frais_origine_local(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = YEAR
+    label = "Charges dans la catégorie Ventes en gros lait frais d'origine local"
 
 class chiffre_affaire_ventes_inferieur_20_millions(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Chiffre d'affaire dans la catégorie ventes inferieur à 20 millions"
+    label = "Chiffre d'affaire dans la catégorie Ventes < 20 millions"
 
 class charges_ventes_inferieur_20_millions(Variable):
     value_type = float
     entity = Entreprise
     definition_period = YEAR
-    label = "Charges dans la catégorie ventes inferieur à 20 millions"
+    label = "Charges dans la catégorie Ventes < 20 millions"
