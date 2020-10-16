b37294d32143d4d9b44c46047585825052eaef2f
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2020-10-15 15:03:33 -1000

    removed formulas for salaries as it induced some errors when not set, and is not used by future application

--- openfisca_pf/variables/entreprise/salaires.py
+++ openfisca_pf/variables/entreprise/salaires.py
@@ -19,7 +19,6 @@ class salaire(Variable):
     label = "Salaire"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-# This variable is a pure input: it doesn't have a formula
 class salaires_tranche_0(Variable):
     value_type = float
     entity = Entreprise
@@ -29,28 +28,28 @@ class salaires_tranche_0(Variable):
     label = "Salaires de la tranche 0 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[1]
-        salaires_i = entreprise.members('salaire', period)
-        salaire_sup_150000 = salaires_i >= 150000
-        # Les salaires < 150000 ne sont pas comptabilisés
-        return entreprise.sum(where(salaire_sup_150000, salaires_i, 0))
+    # def formula(entreprise, period, parameters):
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     salaire_sup_150000 = salaires_i >= 150000
+    #     # Les salaires < 150000 ne sont pas comptabilisés
+    #     return entreprise.sum(where(salaire_sup_150000, salaires_i, 0))
 
-class salaires_inferieurs_150000(Variable):
-    value_type = float
-    entity = Entreprise
-    default_value = 0
-    definition_period = MONTH
-    set_input = set_input_divide_by_period  # Optional attribute. Allows user to declare a salary for a year. OpenFisca will spread the yearly amount over the months contained in the year.
-    label = "Salaires de la tranche 0 de CST"
-    reference = "https://law.gov.example/salary"  # Always use the most official source
+# class salaires_inferieurs_150000(Variable):
+#     value_type = float
+#     entity = Entreprise
+#     default_value = 0
+#     definition_period = MONTH
+#     set_input = set_input_divide_by_period  # Optional attribute. Allows user to declare a salary for a year. OpenFisca will spread the yearly amount over the months contained in the year.
+#     label = "Salaires de la tranche 0 de CST"
+#     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[1]
-        salaires_i = entreprise.members('salaire', period)
-        salaire_sup_150000 = salaires_i >= 150000
-        # Les salaires < 150000 ne sont pas comptabilisés
-        return entreprise.sum(where(salaire_sup_150000, 0, salaires_i))
+#     def formula(entreprise, period, parameters):
+#         seuil = parameters(period).dicp.cst_s.taux.thresholds[1]
+#         salaires_i = entreprise.members('salaire', period)
+#         salaire_sup_150000 = salaires_i >= 150000
+#         # Les salaires < 150000 ne sont pas comptabilisés
+#         return entreprise.sum(where(salaire_sup_150000, 0, salaires_i))
 
 class salaires_tranche_1(Variable):
     value_type = float
@@ -61,15 +60,16 @@ class salaires_tranche_1(Variable):
     label = "Salaires de la tranche 1 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 1
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 1
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     print(salaires_i)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_2(Variable):
@@ -81,15 +81,15 @@ class salaires_tranche_2(Variable):
     label = "Salaires de la tranche 2 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 2
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 2
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_3(Variable):
@@ -101,15 +101,15 @@ class salaires_tranche_3(Variable):
     label = "Salaires de la tranche 3 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
     
-    def formula(entreprise, period, parameters):
-        index_tranche = 3
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 3
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_4(Variable):
@@ -121,15 +121,15 @@ class salaires_tranche_4(Variable):
     label = "Salaires de la tranche 4 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 4
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 4
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_5(Variable):
@@ -141,15 +141,15 @@ class salaires_tranche_5(Variable):
     label = "Salaires de la tranche 5 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 5
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 5
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_6(Variable):
@@ -161,15 +161,15 @@ class salaires_tranche_6(Variable):
     label = "Salaires de la tranche 6 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 6
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 6
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_7(Variable):
@@ -181,15 +181,15 @@ class salaires_tranche_7(Variable):
     label = "Salaires de la tranche 7 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 7
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 7
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_8(Variable):
@@ -201,15 +201,15 @@ class salaires_tranche_8(Variable):
     label = "Salaires de la tranche 8 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 8
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 8
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_9(Variable):
@@ -221,15 +221,15 @@ class salaires_tranche_9(Variable):
     label = "Salaires de la tranche 9 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 9
-        seuil = parameters(period).taxes.taux_cst_s.thresholds[index_tranche + 1]
-        salaires_i = entreprise.members('salaire', period)
-        tranche = min_(salaires_i, seuil)
-        valeur = entreprise.sum(tranche)
-        for i in range(0, index_tranche):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 9
+    #     seuil = parameters(period).dicp.cst_s.taux.thresholds[index_tranche + 1]
+    #     salaires_i = entreprise.members('salaire', period)
+    #     tranche = min_(salaires_i, seuil)
+    #     valeur = entreprise.sum(tranche)
+    #     for i in range(0, index_tranche):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 # This variable is a pure input: it doesn't have a formula
 class salaires_tranche_10(Variable):
@@ -241,13 +241,13 @@ class salaires_tranche_10(Variable):
     label = "Salaires de la tranche 10 de CST"
     reference = "https://law.gov.example/salary"  # Always use the most official source
 
-    def formula(entreprise, period, parameters):
-        index_tranche = 10
-        salaires_i = entreprise.members('salaire', period)
-        valeur = entreprise.sum(salaires_i)
-        for i in range(0, index_tranche - 1):
-            valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
-        return valeur
+    # def formula(entreprise, period, parameters):
+    #     index_tranche = 10
+    #     salaires_i = entreprise.members('salaire', period)
+    #     valeur = entreprise.sum(salaires_i)
+    #     for i in range(0, index_tranche - 1):
+    #         valeur = valeur - entreprise('salaires_tranche_' + str(i), period)
+    #     return valeur
 
 class salaires_totaux(Variable):
     value_type = float
