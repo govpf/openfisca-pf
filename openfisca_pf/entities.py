2f1fc3af7e35d71da384f0e7d13284430bf3a806
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2021-01-07 15:55:23 -1000

    removed useless Personne entity

--- openfisca_pf/entities.py
+++ openfisca_pf/entities.py
@@ -20,23 +20,6 @@ Person = build_entity(
     is_person = True,
     )
 
-Personne = build_entity(
-    key = "personne",
-    plural = "personnes",
-    label = u'An individual. The minimal legal entity on which a legislation might be applied.',
-    doc = '''
-
-    Variables like 'salary' and 'income_tax' are usually defined for the entity 'Person'.
-
-    Usage:
-    Calculate a variable applied to a 'Person' (e.g. access the 'salary' of a specific month with person('salary', "2017-05")).
-    Check the role of a 'Person' in a group entity (e.g. check if a the 'Person' is a 'first_parent' in a 'Household' entity with person.has_role(Household.FIRST_PARENT)).
-
-    For more information, see: https://openfisca.org/doc/coding-the-legislation/50_entities.html
-    ''',
-    is_person = True,
-    )
-
 Entreprise = build_entity(
     key = "entreprise",
     plural = "entreprises",
@@ -63,4 +46,4 @@ Entreprise = build_entity(
         ]
     )
 
-entities = [Person, Personne, Entreprise]
+entities = [Person, Entreprise]
