5300fb025a27f71bd21029347290360ef3ba351c
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2021-02-02 20:02:27 -1000

    adding taux as parameters

---
+++ openfisca_pf/variables/dicp/CSTS_par_tranche.py
@@ -0,0 +1,320 @@
+# -*- coding: utf-8 -*-
+
+# This file defines variables for the modelled legislation.
+# A variable is a property of an Entity such as a Person, a Household…
+# See https://openfisca.org/doc/key-concepts/variables.html
+
+# Import from openfisca-core the common Python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the Entities specifically defined for this tax and benefit system
+from openfisca_pf.entities import *
+import numpy
+
+
+# CSTS par tranche
+class cst_s_due_tranche_1(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 1"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_1', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_1', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_2(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 1"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_2', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_2', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_3(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 2"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_3', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_3', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_4(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 3"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_4', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_4', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_5(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 4"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_5', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_5', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_6(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 5"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_6', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_6', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_7(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 6"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_7', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_7', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_8(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 7"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_8', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_8', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_9(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 8"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_9', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_9', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_10(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 9"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_10', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_10', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class cst_s_due_tranche_11(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"CST-S due par l'entreprise sur l'ensemble des salaires déclarés sur la tranche 10"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        salaires_tranche = entreprise('revenus_tranche_11', period)
+        revenus_tranche_inf_0 = salaires_tranche < 0
+        taux = entreprise('taux_cst_s_tranche_11', period)
+        cst_s_tranche = taux * where(revenus_tranche_inf_0, 0, salaires_tranche)
+        return numpy.rint(numpy.nextafter(cst_s_tranche, cst_s_tranche + 1))
+
+
+class taux_cst_s_tranche_1(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 1"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[0]
+
+
+class taux_cst_s_tranche_2(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 2"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[1]
+
+
+class taux_cst_s_tranche_3(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 3"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[2]
+
+
+class taux_cst_s_tranche_4(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 4"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[3]
+
+
+class taux_cst_s_tranche_5(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 5"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[4]
+
+
+class taux_cst_s_tranche_6(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 6"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[5]
+
+
+class taux_cst_s_tranche_7(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 7"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[6]
+
+
+class taux_cst_s_tranche_8(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 8"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[7]
+
+
+class taux_cst_s_tranche_9(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 9"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[8]
+
+
+class taux_cst_s_tranche_10(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 10"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[9]
+
+
+class taux_cst_s_tranche_11(Variable):
+    value_type = float
+    entity = Entreprise
+    definition_period = MONTH
+    label = u"Taux de la CST-S pour la tranche 11"
+    reference = "https://law.gov.example/income_tax"  # Always use the most official source
+
+    # The formula to compute the income tax for a given person at a given period
+    def formula(entreprise, period, parameters):
+        return parameters(period).dicp.cst_s.taux.rates[10]
