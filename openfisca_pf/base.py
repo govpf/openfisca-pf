4eaed9b227d5ecc1dd090bf4003835f9d40b9c62
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2021-02-09 15:13:13 -1000

    adding arronduSup and inf

--- openfisca_pf/base.py
+++ openfisca_pf/base.py
@@ -2,6 +2,7 @@
 
 # This file defines the base enum needed by our legislation.
 from openfisca_core.model_api import *
+import numpy
 
 
 class TypePersonne(Enum):
@@ -12,3 +13,13 @@ class TypePersonne(Enum):
 class OuiNon(Enum):
     O = u'Oui'
     N = u'Non'
+
+
+# This function round up if result is 0.5 so for ex 1.5 => 2, 1.6 => 2 but 1.4 => 1
+def arrondiSup(valeur):
+    return numpy.rint(numpy.nextafter(valeur, valeur + 1))
+
+
+# This function round down if result is 0.5 so for ex 1.5 => 1, 1.6 => 2 and 1.4 => 1
+def arrondiInf(valeur):
+    return numpy.rint(numpy.nextafter(valeur, valeur - 1))
