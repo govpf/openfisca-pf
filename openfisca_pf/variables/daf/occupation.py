4d7a5f67181674f434e59d2ed421bdc44d4718da
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2021-06-22 15:05:40 -1000

    adding a fake montant_redevance_domaniale and 4 types of calculation

--- openfisca_pf/variables/daf/occupation.py
+++ openfisca_pf/variables/daf/occupation.py
@@ -26,14 +26,21 @@ class ZonesOccupations(Enum):
     Z4 = u'Zone 4'
 
 
-class duree_occupation_domaine(Variable):
+class TypesNatureEmprise(Enum):
+    equipement_du_pays = u'Equipement du pays'
+    terrain_de_sport_avec_electricite = u'Terrain de sport avec électricité'
+    terrain_de_sport_sans_electricite = u'Terrain de sport sans électricité'
+    infrastructure_de_restauration_aeroportuaire = u'Infrastructure de restauration aéroportuaire'
+
+
+class duree_occupation_redevance_domaniale(Variable):
     value_type = int
     entity = Personne
     definition_period = DAY
     label = "Durée de l'occupation du domaine"
 
 
-class unite_duree_occupation_domaine(Variable):
+class unite_duree_occupation_redevance_domaniale(Variable):
     value_type = Enum
     possible_values = UnitesDuree
     default_value = UnitesDuree.Jours
@@ -42,10 +49,74 @@ class unite_duree_occupation_domaine(Variable):
     label = "Unité de la durée d'occupation (heures, jours, mois, années...)"
 
 
-class zone_occupation_domaine(Variable):
+class zone_occupation_redevance_domaniale(Variable):
     value_type = Enum
     possible_values = ZonesOccupations
     default_value = ZonesOccupations.NA
     entity = Personne
     definition_period = DAY
     label = "Zone de l'occupation du domaine"
+
+
+class nature_emprise_occupation_redevance_domaniale(Variable):
+    value_type = Enum
+    possible_values = TypesNatureEmprise
+    default_value = TypesNatureEmprise.equipement_du_pays
+    entity = Personne
+    definition_period = DAY
+    label = "Type de nature d'emprise"
+
+
+class montant_redevance_domaniale(Variable):
+    value_type = float
+    entity = Personne
+    definition_period = DAY
+    label = "Montant de la redevance domaniale dûe"
+
+    def formula(personne, period, parameters):
+        nature_emprise_occupation_redevance_domaniale = personne('nature_emprise_occupation_redevance_domaniale', period)
+        return select([nature_emprise_occupation_redevance_domaniale == TypesNatureEmprise.equipement_du_pays,
+            nature_emprise_occupation_redevance_domaniale == TypesNatureEmprise.terrain_de_sport_avec_electricite,
+            nature_emprise_occupation_redevance_domaniale == TypesNatureEmprise.terrain_de_sport_avec_electricite,
+            nature_emprise_occupation_redevance_domaniale == TypesNatureEmprise.infrastructure_de_restauration_aeroportuaire],
+        [personne('montant_redevance_domaniale_type_1', period), personne('montant_redevance_domaniale_type_2', period), personne('montant_redevance_domaniale_type_3', period), personne('montant_redevance_domaniale_type_4', period)])
+
+
+class montant_redevance_domaniale_type_1(Variable):
+    value_type = float
+    entity = Personne
+    definition_period = DAY
+    label = "Montant de la redevance domaniale dûe calcul type 1"
+
+    def formula(personne, period, parameters):
+        return 1
+
+
+class montant_redevance_domaniale_type_2(Variable):
+    value_type = float
+    entity = Personne
+    definition_period = DAY
+    label = "Montant de la redevance domaniale dûe calcul type 2"
+
+    def formula(personne, period, parameters):
+        return 2
+
+
+class montant_redevance_domaniale_type_3(Variable):
+    value_type = float
+    entity = Personne
+    definition_period = DAY
+    label = "Montant de la redevance domaniale dûe calcul type 3"
+
+    def formula(personne, period, parameters):
+        return 3
+
+
+class montant_redevance_domaniale_type_4(Variable):
+    value_type = float
+    entity = Personne
+    definition_period = DAY
+    label = "Montant de la redevance domaniale dûe calcul type 4"
+
+    def formula(personne, period, parameters):
+        return 4
