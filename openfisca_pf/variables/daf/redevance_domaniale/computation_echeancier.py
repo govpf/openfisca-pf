7b4ccb1f0bd829fe974daf4ab0fdc84b357ca8cd
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2021-07-13 12:21:46 -1000

    echancier_annee is now calculated on it's own

--- openfisca_pf/variables/daf/redevance_domaniale/computation_echeancier.py
+++ openfisca_pf/variables/daf/redevance_domaniale/computation_echeancier.py
@@ -11,13 +11,13 @@ from openfisca_pf.entities import *
 
 from numpy import datetime64, timedelta64
 from openfisca_pf.base import *
-
+from openfisca_core import periods
 
 class montant_redevance_domaniale_echeancier_jour(Variable):
     value_type = float
     entity = Personne
     definition_period = DAY
-    # calculate_output = calculate_output_add
+    calculate_output = calculate_output_add
 
     def formula(personne, period, parameters):
         nature_emprise_occupation_redevance_domaniale_echeancier = personne('nature_emprise_occupation_redevance_domaniale_echeancier', period)
@@ -43,30 +43,42 @@ class montant_redevance_domaniale_echeancier_annee(Variable):
     value_type = float
     entity = Personne
     definition_period = YEAR
-    # calculate_output = calculate_output_add
+    calculate_output = calculate_output_add
 
     def formula(personne, period, parameters):
-        # duree_occupation_redevance_domaniale_echeancier = personne('duree_occupation_redevance_domaniale_echeancier', period)
         nature_emprise_occupation_redevance_domaniale_echeancier = personne('nature_emprise_occupation_redevance_domaniale_echeancier', period)
-        # unite_insecable = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].unite_insecable
-        # nb_periode_mini =  numpy.ceil(duree_occupation_redevance_domaniale_echeancier/unite_insecable) ##Use of ceil aims at taking into account that a started period has to be counted in the price
+        variable_redevance_domaniale_echeancier = personne('variable_redevance_domaniale_echeancier', period)
+        nombre_unite_redevance_domaniale_echeancier = personne('nombre_unite_redevance_domaniale_echeancier', period)
+        # majoration_redevance_domaniale = personne('majoration_redevance_domaniale', period)
+        duree_occupation_redevance_domaniale_echeancier = personne('duree_occupation_redevance_domaniale_echeancier', period)
+        unite_insecable = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].unite_insecable
+        nb_periode_mini = numpy.ceil(duree_occupation_redevance_domaniale_echeancier / unite_insecable)
+        date_validation_redevance_domaniale_echeancier = personne('date_validation_redevance_domaniale_echeancier', period)
+        # epsilon = timedelta64(1)
+        date_deb_redevance = numpy.array(date_validation_redevance_domaniale_echeancier, dtype='datetime64')
+        date_fin_redevance = date_deb_redevance + numpy.array(nb_periode_mini * unite_insecable, dtype='timedelta64[D]')
+        date_debut_annee = datetime64(period.start) - 1
+        date_fin_annee = datetime64(period.offset(1, 'year').start) - 1
+        duree = min_(date_fin_redevance, date_fin_annee) - max_(date_deb_redevance, date_debut_annee)
+        # print((duree > timedelta64(0)) * duree)
+        
+        nombre_jours_factures = ((duree > timedelta64(0)) * duree).astype('timedelta64[D]').astype(int)
+
+        # Récupération des paramètres
+        part_fixe = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].part_fixe
+        part_unitaire = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].part_unitaire
+        part_variable = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].part_variable
         montant_minimum = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].montant_minimum
+        # nb_periode_mini = numpy.ceil(nombre_jours_factures / unite_insecable) # Use of ceil aims at taking into account that a started period has to be counted in the price
+        # print(nb_periode_mini)
+        # Calcul du montant
+        montant_intermediaire = (part_fixe +
+                        part_unitaire * nombre_unite_redevance_domaniale_echeancier +
+                        part_variable * variable_redevance_domaniale_echeancier) * nombre_jours_factures / unite_insecable
+                        #* nb_periode_mini
 
-        total = arrondiSup(personne('montant_redevance_domaniale_echeancier_jour', period, options = [ADD]))
-        return max_(total, montant_minimum )
+        # Comparaison avec le minimum
+        montant_global = arrondiSup(max_(montant_intermediaire, nombre_jours_factures / unite_insecable * montant_minimum))
+        # + majoration_redevance_domaniale
 
-
-# class montant_redevance_domaniale_echeancier_total(Variable):
-#     value_type = float
-#     entity = Personne
-#     definition_period = ETERNITY
-
-#     def formula(personne, period, parameters):
-#         # duree_occupation_redevance_domaniale_echeancier = personne('duree_occupation_redevance_domaniale_echeancier', period)
-#         # nature_emprise_occupation_redevance_domaniale_echeancier = personne('nature_emprise_occupation_redevance_domaniale_echeancier', period)
-#         # unite_insecable = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].unite_insecable
-#         # nb_periode_mini =  numpy.ceil(duree_occupation_redevance_domaniale_echeancier/unite_insecable) ##Use of ceil aims at taking into account that a started period has to be counted in the price
-#         # montant_minimum = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale_echeancier].montant_minimum
-
-#         total = personne('montant_redevance_domaniale_echeancier_annee', period, options = [ADD])
-#         return total
+        return montant_global
