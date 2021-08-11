075573905551f13777f665f265dcf5f49d76a4b8
Matthieu Bosc <matthieu.bosc@informatique.gov.pf>
2021-08-10 14:44:40 -1000

    computations by type now return 0 it type is not the same

--- openfisca_pf/variables/daf/redevance_domaniale/computation_type_1.py
+++ openfisca_pf/variables/daf/redevance_domaniale/computation_type_1.py
@@ -17,6 +17,7 @@ from openfisca_pf.entities import *
 from openfisca_pf.variables.daf.redevance_domaniale.enums import *
 from openfisca_pf.base import *
 
+
 class montant_base_redevance_domaniale_type_1(Variable):
     value_type = float
     entity = Personne
@@ -25,28 +26,30 @@ class montant_base_redevance_domaniale_type_1(Variable):
     reference = "Arrêté NOR DAF2120267AC-3"
 
     def formula(personne, period, parameters):
-        ##Déclaration des variables
+        # Déclaration des variables
+        type_calcul = personne('type_calcul_redevance_domaniale', period)
         nature_emprise_occupation_redevance_domaniale = personne('nature_emprise_occupation_redevance_domaniale', period)
+        nature_emprise_occupation_redevance_domaniale = where(type_calcul == '1', nature_emprise_occupation_redevance_domaniale.decode_to_str(), 'equipement_du_pays')
         variable_redevance_domaniale = personne('variable_redevance_domaniale', period)
         nombre_unite_redevance_domaniale = personne('nombre_unite_redevance_domaniale', period)
-        duree_occupation_redevance_domaniale_jour = personne('duree_occupation_redevance_domaniale_jour', period)
-        majoration_redevance_domaniale = personne('majoration_redevance_domaniale', period)
+        # duree_occupation_redevance_domaniale_jour = personne('duree_occupation_redevance_domaniale_jour', period)
+        # majoration_redevance_domaniale = personne('majoration_redevance_domaniale', period)
 
-        ##Récupération des paramètres
+        # Récupération des paramètres
         part_fixe = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale].part_fixe
         part_unitaire = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale].part_unitaire
         part_variable = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale].part_variable
         montant_minimum = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale].montant_minimum
 
-        ##Calcul du montant
+        # Calcul du montant
         montant_intermediaire = (part_fixe +
                         part_unitaire * nombre_unite_redevance_domaniale +
                         part_variable * variable_redevance_domaniale)
 
-        ##Comparaison avec le minimum
-        montant_base= max_(arrondiSup(montant_intermediaire), montant_minimum )
+        # Comparaison avec le minimum
+        montant_base = max_(arrondiSup(montant_intermediaire), montant_minimum)
 
-        return montant_base
+        return where(type_calcul == '1', montant_base, 0)
 
 
 class montant_total_redevance_domaniale_type_1(Variable):
@@ -57,16 +60,18 @@ class montant_total_redevance_domaniale_type_1(Variable):
     reference = "Arrêté NOR DAF2120267AC-3"
 
     def formula(personne, period, parameters):
-        ##Déclaration des variables
+        # Déclaration des variables
+        type_calcul = personne('type_calcul_redevance_domaniale', period)
         nature_emprise_occupation_redevance_domaniale = personne('nature_emprise_occupation_redevance_domaniale', period)
+        nature_emprise_occupation_redevance_domaniale = where(type_calcul == '1', nature_emprise_occupation_redevance_domaniale.decode_to_str(), 'equipement_du_pays')
         duree_occupation_redevance_domaniale_jour = personne('duree_occupation_redevance_domaniale_jour', period)
         majoration_redevance_domaniale = personne('majoration_redevance_domaniale', period)
-        montant_base = personne('montant_base_redevance_domaniale_type_1',period)
-        
-        ##Récupération des paramètres
+        montant_base = personne('montant_base_redevance_domaniale_type_1', period)
+
+        # Récupération des paramètres
         base_calcul_jour = parameters(period).daf.redevance_domaniale.type_1[nature_emprise_occupation_redevance_domaniale].base_calcul_jour
 
-        ##Calcul du montant total sur toute la durée de l'occupation
+        # Calcul du montant total sur toute la durée de l'occupation
         montant_total = arrondiSup(montant_base * duree_occupation_redevance_domaniale_jour / base_calcul_jour) + majoration_redevance_domaniale
 
-        return montant_total
+        return where(type_calcul == '1', montant_total, 0)
--- openfisca_pf/variables/daf/redevance_domaniale/computation_type_2.py
+++ openfisca_pf/variables/daf/redevance_domaniale/computation_type_2.py
@@ -11,6 +11,7 @@ from openfisca_pf.entities import *
 from openfisca_pf.variables.daf.redevance_domaniale.enums import *
 from openfisca_pf.base import *
 
+
 class montant_base_redevance_domaniale_type_2(Variable):
     value_type = float
     entity = Personne
@@ -19,27 +20,30 @@ class montant_base_redevance_domaniale_type_2(Variable):
     reference = "Arrêté NOR DAF2120267AC-3"
 
     def formula(personne, period, parameters):
-        ##Déclaration des variables
+        # Déclaration des variables
+        type_calcul = personne('type_calcul_redevance_domaniale', period)
         nature_emprise_occupation_redevance_domaniale = personne('nature_emprise_occupation_redevance_domaniale', period)
+        nature_emprise_occupation_redevance_domaniale = where(type_calcul == '2', nature_emprise_occupation_redevance_domaniale.decode_to_str(), 'activite_lucrative_manif_sport')
+
         variable_redevance_domaniale = personne('variable_redevance_domaniale', period)
         nombre_unite_redevance_domaniale = personne('nombre_unite_redevance_domaniale', period)
         zone_occupation_redevance_domaniale = personne('zone_occupation_redevance_domaniale', period)
 
-        ##Récupération des paramètres
+        # Récupération des paramètres
         part_fixe = parameters(period).daf.redevance_domaniale.type_2[nature_emprise_occupation_redevance_domaniale][zone_occupation_redevance_domaniale].part_fixe
         part_unitaire = parameters(period).daf.redevance_domaniale.type_2[nature_emprise_occupation_redevance_domaniale][zone_occupation_redevance_domaniale].part_unitaire
         part_variable = parameters(period).daf.redevance_domaniale.type_2[nature_emprise_occupation_redevance_domaniale][zone_occupation_redevance_domaniale].part_variable
         montant_minimum = parameters(period).daf.redevance_domaniale.type_2[nature_emprise_occupation_redevance_domaniale][zone_occupation_redevance_domaniale].montant_minimum
         
-        ##Calcul du montant
-        montant_intermediaire = (part_fixe + 
+        # Calcul du montant
+        montant_intermediaire = (part_fixe +
                         part_unitaire * nombre_unite_redevance_domaniale +
                         part_variable * variable_redevance_domaniale)
 
-        ##Comparaison avec le minimum
-        montant_base= max_(arrondiSup(montant_intermediaire), montant_minimum )
+        # Comparaison avec le minimum
+        montant_base = max_(arrondiSup(montant_intermediaire), montant_minimum)
+        return where(type_calcul == '2', montant_base, 0)
 
-        return montant_base
 
 class montant_total_redevance_domaniale_type_2(Variable):
     value_type = float
@@ -49,17 +53,17 @@ class montant_total_redevance_domaniale_type_2(Variable):
     reference = "Arrêté NOR DAF2120267AC-3"
 
     def formula(personne, period, parameters):
-        ##Déclaration des variables
+        # Déclaration des variables
+        type_calcul = personne('type_calcul_redevance_domaniale', period)
         nature_emprise_occupation_redevance_domaniale = personne('nature_emprise_occupation_redevance_domaniale', period)
+        nature_emprise_occupation_redevance_domaniale = where(type_calcul == '2', nature_emprise_occupation_redevance_domaniale.decode_to_str(), 'activite_lucrative_manif_sport')
         duree_occupation_redevance_domaniale_jour = personne('duree_occupation_redevance_domaniale_jour', period)
         majoration_redevance_domaniale = personne('majoration_redevance_domaniale', period)
-        montant_base = personne('montant_base_redevance_domaniale_type_2',period)
+        montant_base = personne('montant_base_redevance_domaniale_type_2', period)
         zone_occupation_redevance_domaniale = personne('zone_occupation_redevance_domaniale', period)
-
-        ##Récupération des paramètres
+        # Récupération des paramètres
         base_calcul_jour = parameters(period).daf.redevance_domaniale.type_2[nature_emprise_occupation_redevance_domaniale][zone_occupation_redevance_domaniale].base_calcul_jour
 
-        ##Calcul du montant total sur toute la durée de l'occupation
+        # Calcul du montant total sur toute la durée de l'occupation
         montant_total = arrondiSup(montant_base * duree_occupation_redevance_domaniale_jour / base_calcul_jour) + majoration_redevance_domaniale
-
-        return montant_total
+        return where(type_calcul == '2', montant_total, 0)
