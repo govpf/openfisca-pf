04316465ace9bbff5338efc5e1a039f0d05fb348
masterbox <matthieu.bosc@informatique.gov.pf>
2022-04-14 13:35:06 -1000

    updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-PF",
-    version = "0.6.0",
+    version = "0.6.1",
     author = "SIPf",
     author_email = "matthieu.bosc@informatique.gov.pf",
     classifiers=[
