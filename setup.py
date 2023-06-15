049e584cea48bcd201c619aca398caa068f60a58
Philippe Pérez de San Roman (DSI) <philippe.perezdesanroman@administration.gov.pf>
2023-06-15 13:39:34 -1000

    feature/python_3.11_and_openfisca_core_40.0.0 (#82)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version = "0.6.2",
     author = "SIPf",
     author_email = "matthieu.bosc@informatique.gov.pf",
-    classifiers=[
+    classifiers = [
         "Development Status :: 1 - Planning",
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: POSIX",
@@ -16,14 +16,14 @@ setup(
         ],
     description = "OpenFisca tax system for PF",
     keywords = "microsimulation tax calculation",
-    license ="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    license = "http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     url = "https://github.com/govpf/openfisca-pf",
     include_package_data = True,  # Will read MANIFEST.in
     data_files = [
         ("share/openfisca/openfisca-country-template", ["CHANGELOG.md", "LICENSE", "README.md"]),
         ],
     install_requires = [
-        "OpenFisca-Core[web-api] >=27.0,<40.0",
+        "OpenFisca-Core[web-api] ==40.0.0",
         ],
     extras_require = {
         "dev": [
@@ -33,5 +33,5 @@ setup(
             "pycodestyle >=2.3.0,<2.6.0",  # To avoid incompatibility with flake
             ]
         },
-    packages=find_packages(),
+    packages = find_packages(),
     )
