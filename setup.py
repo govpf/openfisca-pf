69313f58dfa423d2deea2f230c291b1e20919b06
pperezdesa <philippe.perezdesanroman@administration.gov.pf>
2023-06-15 10:33:54 -1000

    allez la!

--- setup.py
+++ setup.py
@@ -14,24 +14,24 @@ setup(
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: Information Analysis",
         ],
-    description = "OpenFisca tax system for PF",
-    keywords = "microsimulation tax calculation",
-    license ="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
-    url = "https://github.com/govpf/openfisca-pf",
-    include_package_data = True,  # Will read MANIFEST.in
-    data_files = [
+    description="OpenFisca tax system for PF",
+    keywords="microsimulation tax calculation",
+    license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
+    url="https://github.com/govpf/openfisca-pf",
+    include_package_data=True,  # Will read MANIFEST.in
+    data_files=[
         ("share/openfisca/openfisca-country-template", ["CHANGELOG.md", "LICENSE", "README.md"]),
-        ],
-    install_requires = [
+    ],
+    install_requires=[
         "OpenFisca-Core[web-api] ==40.0.0",
     ],
-    extras_require = {
+    extras_require={
         "dev": [
             "autopep8 ==1.5",
             "flake8 >=3.5.0,<3.8.0",
             "flake8-print",
             "pycodestyle >=2.3.0,<2.6.0",  # To avoid incompatibility with flake
-            ]
-        },
+        ]
+    },
     packages=find_packages(),
-    )
+)
