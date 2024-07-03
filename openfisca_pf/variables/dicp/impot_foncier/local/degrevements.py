909a14630d3b76ba11df16aafd5d1f6441752ec7
AlexisDefranoux <25173038+AlexisDefranoux@users.noreply.github.com>
2024-07-03 12:55:00 -1000

    Feature/fon 244 valeur locative (#104)
    * Add type_local and refactor abattement / degrevement
    
    * Fix linter
    
    * Add autre
    
    ---------
    
    Co-authored-by: Alexis <alexis.defranoux@informatique.gov.pf>

--- openfisca_pf/variables/dicp/impot_foncier/local/degrevements.py
+++ openfisca_pf/variables/dicp/impot_foncier/local/degrevements.py
@@ -2,56 +2,8 @@
 
 import numpy
 
-from openfisca_pf.entities import Personne
 from openfisca_pf.base import YEAR, Variable
-
-
-class premier_degrevement(Variable):
-    value_type = float
-    entity = Personne
-    definition_period = YEAR
-    default_value = 0
-    label = "Premier dégrèvement appliqué à la valeur locative pour calculer la base imposable de l'impôt foncier"
-    reference = "https://lexpol.cloud.pf/LexpolAfficheTexte.php?texte=581595"
-
-    def formula(local, period, parameters):
-        return local.pays('premier_degrevement_pays', period, parameters)
-
-
-class second_degrevement_si_non_loue(Variable):
-    value_type = float
-    entity = Personne
-    definition_period = YEAR
-    default_value = 0
-    label = "Second dégrèvement appliqué pour calculer la base imposable de l'impôt foncier si le local n'est pas loué"
-    reference = "https://lexpol.cloud.pf/LexpolAfficheTexte.php?texte=581595"
-
-    def formula(local, period, parameters):
-        return local.pays('second_degrevement_si_non_loue_pays', period, parameters)
-
-
-class second_degrevement_si_loue_meuble(Variable):
-    value_type = float
-    entity = Personne
-    definition_period = YEAR
-    default_value = 0
-    label = "Second dégrèvement appliqué pour calculer la base imposable de l'impôt foncier si le local est loué en meublé"
-    reference = "https://lexpol.cloud.pf/LexpolAfficheTexte.php?texte=581595"
-
-    def formula(local, period, parameters):
-        return local.pays('second_degrevement_si_loue_meuble_pays', period, parameters)
-
-
-class second_degrevement_si_loue_non_meuble(Variable):
-    value_type = float
-    entity = Personne
-    definition_period = YEAR
-    default_value = 0
-    label = "Second dégrèvement appliqué pour calculer la base imposable de l'impôt foncier si le local est loué en non-meublé"
-    reference = "https://lexpol.cloud.pf/LexpolAfficheTexte.php?texte=581595"
-
-    def formula(local, period, parameters):
-        return local.pays('second_degrevement_si_loue_non_meuble_pays', period, parameters)
+from openfisca_pf.entities import Personne
 
 
 class degrevement_pour_baisse_de_revenus_loue_en_meuble_de_tourisme(Variable):
