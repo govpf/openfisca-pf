c365d9f9fdc6dcb8dd67f2e2417170515bd20ed7
xpallot <xpallot@etiskapp.com>
2025-03-26 15:42:36 +0100

    Adjust IMF + refactoring.

--- openfisca_pf/variables/dicp/impot_societe/impots_brut/imf.py
+++ openfisca_pf/variables/dicp/impot_societe/impots_brut/imf.py
@@ -1,10 +1,10 @@
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_pf.base import (Period, DAY, select, Variable, where, isin, not_)
 from openfisca_pf.entities import Personne
-from openfisca_pf.enums.impot_societe.activity import Activite, ACTIVITE_EXONERATRICE_TAUX_A_SAISIR
+from openfisca_pf.enums.impot_societe.activity import Activite, ACTIVITE_ABATTEMENT_IMF, ACTIVITE_ABATTEMENT_TAUX_A_SAISIR_IMF
 
 
-class is_imf_base(Variable):
+class imf_brut_base(Variable):
     value_type = int
     entity = Personne
     definition_period = DAY
@@ -18,7 +18,7 @@ class is_imf_base(Variable):
         return produits_exploitation - transfert_charge_exploitation + produits_financier - transfert_charge_financier
 
 
-class is_imf_taux_benefice(Variable):
+class imf_brut_taux_benefice(Variable):
     value_type = float
     entity = Personne
     definition_period = DAY
@@ -29,7 +29,7 @@ class is_imf_taux_benefice(Variable):
         return taux
 
 
-class is_imf_taux_deficit(Variable):
+class imf_brut_taux_deficit(Variable):
     value_type = float
     entity = Personne
     definition_period = DAY
@@ -40,7 +40,7 @@ class is_imf_taux_deficit(Variable):
         return taux
 
 
-class is_imf_est_deficitaire(Variable):
+class imf_est_deficitaire(Variable):
     value_type = bool
     entity = Personne
     definition_period = DAY
@@ -51,27 +51,27 @@ class is_imf_est_deficitaire(Variable):
         return deficit > 0
 
 
-class is_imf_est_beneficiaire(Variable):
+class imf_est_beneficiaire(Variable):
     value_type = bool
     entity = Personne
     definition_period = DAY
     label = "L'IMF est-il bénéficiare ?"
 
     def formula(person: Personne, period, parameters):
-        deficitaire = person('is_imf_est_deficitaire', period)
+        deficitaire = person('imf_est_deficitaire', period)
         return not_(deficitaire)
 
 
-class is_imf_taux(Variable):
+class imf_brut_taux(Variable):
     value_type = float
     entity = Personne
     definition_period = DAY
     label = "Taux IMF à la date de clotûre"
 
     def formula(person: Personne, period, parameters):
-        taux_benefice = person('is_imf_taux_benefice', period)
-        taux_deficit = person('is_imf_taux_deficit', period)
-        deficitaire = person('is_imf_est_deficitaire', period)
+        taux_benefice = person('imf_brut_taux_benefice', period)
+        taux_deficit = person('imf_brut_taux_deficit', period)
+        deficitaire = person('imf_est_deficitaire', period)
         taux = where(deficitaire, taux_deficit, taux_benefice)
         return taux
 
@@ -84,7 +84,7 @@ class is_nombre_mois_exercice(Variable):
     default_value = 12
 
 
-class is_imf_montant_minimum(Variable):
+class imf_brut_montant_minimum(Variable):
     value_type = int
     entity = Personne
     definition_period = DAY
@@ -96,7 +96,7 @@ class is_imf_montant_minimum(Variable):
         return (montant / 12) * nombre_mois
 
 
-class is_imf_montant_maximum(Variable):
+class imf_brut_montant_maximum(Variable):
     value_type = int
     entity = Personne
     definition_period = DAY
@@ -108,14 +108,26 @@ class is_imf_montant_maximum(Variable):
         return (montant / 12) * nombre_mois
 
 
-class is_imf_abattement_taux_saisie(Variable):
+class imf_brut_possede_abattement(Variable):
+    value_type = bool
+    entity = Personne
+    definition_period = DAY
+    default_value = False
+    label = "Activité possède un abattement IMF ?"
+
+    def formula(local: Personne, period: Period, parameters):
+        activite_principale = local('is_activite_principale', period, parameters)
+        return isin(activite_principale, ACTIVITE_ABATTEMENT_IMF)
+
+
+class imf_brut_abattement_taux_saisie(Variable):
     value_type = float
     entity = Personne
     definition_period = DAY
     label = "Taux IMF Abattement Saisie"
 
 
-class is_imf_abattement_taux_est_a_saisir(Variable):
+class imf_brut_abattement_taux_est_a_saisir(Variable):
     value_type = bool
     entity = Personne
     definition_period = DAY
@@ -123,10 +135,10 @@ class is_imf_abattement_taux_est_a_saisir(Variable):
 
     def formula(local: Personne, period: Period, parameters):
         activite_principale = local('is_activite_principale', period, parameters)
-        return isin(activite_principale, ACTIVITE_EXONERATRICE_TAUX_A_SAISIR)
+        return isin(activite_principale, ACTIVITE_ABATTEMENT_TAUX_A_SAISIR_IMF)
 
 
-class is_imf_abattement_taux(Variable):
+class imf_brut_abattement_taux(Variable):
     value_type = float
     entity = Personne
     definition_period = DAY
@@ -134,44 +146,21 @@ class is_imf_abattement_taux(Variable):
 
     def formula(person: Personne, period, parameters):
         activite_principale = person('is_activite_principale', period)
-        possede_abattement = person('is_activite_principale_possede_abattement', period)
-        abattement_taux_est_a_saisir = person('is_imf_abattement_taux_est_a_saisir', period)
-        abattement_saisie = person('is_imf_abattement_taux_saisie', period)
+        possede_abattement = person('imf_brut_possede_abattement', period)
+        abattement_taux_est_a_saisir = person('imf_brut_abattement_taux_est_a_saisir', period)
+        abattement_saisie = person('imf_brut_abattement_taux_saisie', period)
 
         nbr_exercice = person('is_nombre_exercices', period)
         nbr_exercice = where(nbr_exercice > 0, nbr_exercice, 999)
 
         # NotImplementedError returned by "parameters(period).dicp.impot_societe.taux.activite_exoneration_is[activite_principale]"
-        societes_gestion_fonds_garantie = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.SOCIETES_GESTION_FONDS_GARANTIE
-        activites_croisieres = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.ACTIVITES_CROISIERE
-        concessions_minieres = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.CONCESSIONS_MINIERES
-        membre_groupe_fiscal = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.MEMBRE_GROUPE_FISCAL
-        gie = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.GIE
-        scpr = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.SCPR
-        scm = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.SCM
-        obnl = parameters(period).dicp.impot_societe.taux.imf_exoneration_is.OBNL
+        societes_gestion_fonds_garantie = parameters(period).dicp.impot_societe.taux.imf_abattement.SOCIETES_GESTION_FONDS_GARANTIE
 
         abattement = select([
             activite_principale == Activite.SOCIETES_GESTION_FONDS_GARANTIE,
-            activite_principale == Activite.ACTIVITES_CROISIERE,
-            activite_principale == Activite.CONCESSIONS_MINIERES,
-            activite_principale == Activite.MEMBRE_GROUPE_FISCAL,
-            activite_principale == Activite.GIE,
-            activite_principale == Activite.SCPR,
-            activite_principale == Activite.SCM,
-            activite_principale == Activite.OBNL,
-            True
             ], [
             societes_gestion_fonds_garantie.calc(nbr_exercice),
-            activites_croisieres.calc(nbr_exercice),
-            concessions_minieres.calc(nbr_exercice),
-            membre_groupe_fiscal.calc(nbr_exercice),
-            gie.calc(nbr_exercice),
-            scpr.calc(nbr_exercice),
-            scm.calc(nbr_exercice),
-            obnl.calc(nbr_exercice),
-            0.0
-            ])
+            ], 0.0)
 
         abattement = where(abattement_taux_est_a_saisir, abattement_saisie, abattement)
         abattement = where(possede_abattement, abattement, 0)
@@ -179,26 +168,35 @@ class is_imf_abattement_taux(Variable):
         return abattement
 
 
-class is_imf_montant(Variable):
+class imf_brut_base_imposable(Variable):
     value_type = int
     entity = Personne
     definition_period = DAY
-    label = "Montant IMF"
+    label = "Base IMF Brut Imposable"
 
     def formula(person: Personne, period, parameters):
-        imf_base = person('is_imf_base', period)
-        taux = person('is_imf_taux', period)
-        abattement = person('is_imf_abattement_taux', period)
-        imf_due = imf_base * (1 - abattement) * taux
-        montant_min = person('is_imf_montant_minimum', period)
-        montant_max = person('is_imf_montant_maximum', period)
+        base = person('imf_brut_base', period)
+        abattement = person('imf_brut_abattement_taux', period)
+        return base * (1 - abattement)
+
+
+class imf_brut_due(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Montant IMF brut due"
+
+    def formula(person: Personne, period, parameters):
+        base_imposable = person('imf_brut_base_imposable', period)
+        taux = person('imf_brut_taux', period)
+        imf_due = base_imposable * taux
+        montant_min = person('imf_brut_montant_minimum', period)
+        montant_max = person('imf_brut_montant_maximum', period)
         montant = select([
             imf_due < montant_min,
-            montant_max < imf_due,
-            True
+            montant_max < imf_due
             ], [
             montant_min,
-            montant_max,
-            imf_due
-            ])
+            montant_max
+            ], imf_due)
         return montant
--- openfisca_pf/variables/dicp/impot_societe/impots_brut/is.py
+++ openfisca_pf/variables/dicp/impot_societe/impots_brut/is.py
@@ -1,23 +1,8 @@
 # Import the Entities specifically defined for this tax and benefit system
-from openfisca_pf.base import (
-    ArrayLike,
-    DAY,
-    Enum,
-    isin,
-    ParameterNode,
-    Period,
-    Population,
-    select,
-    Variable,
-    where
-    )
+from openfisca_pf.base import (ArrayLike, Period, DAY, Enum, select, Variable, where, isin, ParameterNode, Population)
 from openfisca_pf.entities import Personne
-from openfisca_pf.enums.impot_societe.activity import (
-    Activite,
-    ACTIVITE_TAUX_IS,
-    ACTIVITE_EXONERATRICE,
-    ACTIVITE_EXONERATRICE_TAUX_A_SAISIR
-    )
+from openfisca_pf.enums.impot_societe.activity import Activite, ACTIVITE_TAUX_IS, ACTIVITE_ABATTEMENT_IS, \
+    ACTIVITE_ABATTEMENT_TAUX_A_SAISIR_IS
 from openfisca_pf.functions.currency import arrondi_millier_inferieur
 
 
@@ -30,7 +15,7 @@ class is_activite_principale(Variable):
     label = "Activité principale societe"
 
 
-class is_activite_principale_possede_taux_is(Variable):
+class is_brut_possede_taux_is(Variable):
     value_type = bool
     entity = Personne
     definition_period = DAY
@@ -38,7 +23,7 @@ class is_activite_principale_possede_taux_is(Variable):
     label = "Activité possède un taux IS ?"
 
     def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
-        activite_principale = personne('is_activite_principale', period)
+        activite_principale = personne('is_activite_principale', period, parameters)
         return isin(activite_principale, ACTIVITE_TAUX_IS)
 
 
@@ -74,9 +59,9 @@ class is_brut_taux_activite(Variable):
 
     def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
         activite_principale = personne('is_activite_principale', period)
-        possede_taux_is = personne('is_activite_principale_possede_taux_is', period)
+        possede_taux_is = personne('is_brut_possede_taux_is', period)
         activite_taux = where(possede_taux_is, activite_principale.decode_to_str(), 'NORMALE')
-        taux_principal = parameters(period).dicp.impot_societe.taux.activite[activite_taux]
+        taux_principal = parameters(period).dicp.impot_societe.taux.is_par_activite[activite_taux]
         return taux_principal
 
 
@@ -90,11 +75,11 @@ class is_brut_taux(Variable):
         taux_principal = personne('is_brut_taux_activite', period)
         is_zrae = personne('is_est_zrae', period)
         quotient_zrae = personne('is_quotient_zrae', period)
-        taux_zrae = parameters(period).dicp.impot_societe.taux.activite['zone_revitalisation_zrae']
+        taux_zrae = parameters(period).dicp.impot_societe.taux.is_par_activite['zone_revitalisation_zrae']
         return where(is_zrae, (taux_principal * (1 - quotient_zrae) + (taux_zrae * quotient_zrae)), taux_principal)
 
 
-class is_activite_principale_possede_abattement(Variable):
+class is_brut_possede_abattement(Variable):
     value_type = bool
     entity = Personne
     definition_period = DAY
@@ -102,8 +87,8 @@ class is_activite_principale_possede_abattement(Variable):
     label = "Activité possède un abattement IS ?"
 
     def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
-        activite_principale = personne('is_activite_principale', period)
-        return isin(activite_principale, ACTIVITE_EXONERATRICE)
+        activite_principale = personne('is_activite_principale', period, parameters)
+        return isin(activite_principale, ACTIVITE_ABATTEMENT_IS)
 
 
 class is_brut_abattement_taux_saisie(Variable):
@@ -128,8 +113,8 @@ class is_brut_abattement_taux_est_a_saisir(Variable):
     label = "Taux Abattement doit être saisie"
 
     def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
-        activite_principale = personne('is_activite_principale', period)
-        return isin(activite_principale, ACTIVITE_EXONERATRICE_TAUX_A_SAISIR)
+        activite_principale = personne('is_activite_principale', period, parameters)
+        return isin(activite_principale, ACTIVITE_ABATTEMENT_TAUX_A_SAISIR_IS)
 
 
 class is_brut_abattement_taux(Variable):
@@ -140,7 +125,7 @@ class is_brut_abattement_taux(Variable):
 
     def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
         activite_principale = personne('is_activite_principale', period)
-        possede_abattement = personne('is_activite_principale_possede_abattement', period)
+        possede_abattement = personne('is_brut_possede_abattement', period)
         abattement_taux_est_a_saisir = personne('is_brut_abattement_taux_est_a_saisir', period)
         abattement_saisie = personne('is_brut_abattement_taux_saisie', period)
 
@@ -148,36 +133,13 @@ class is_brut_abattement_taux(Variable):
         nombre_exercices = where(nombre_exercices > 0, nombre_exercices, 999)
 
         # NotImplementedError returned by "parameters(period).dicp.impot_societe.taux.activite_exoneration_is[activite_principale]"
-        societes_gestion_fonds_garantie = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.SOCIETES_GESTION_FONDS_GARANTIE
-        activites_croisieres = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.ACTIVITES_CROISIERE
-        concessions_minieres = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.CONCESSIONS_MINIERES
-        membre_groupe_fiscal = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.MEMBRE_GROUPE_FISCAL
-        gie = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.GIE
-        scpr = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.SCPR
-        scm = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.SCM
-        obnl = parameters(period).dicp.impot_societe.taux.activite_exoneration_is.OBNL
+        societes_gestion_fonds_garantie = parameters(period).dicp.impot_societe.taux.is_abattement.SOCIETES_GESTION_FONDS_GARANTIE
 
         abattement = select([
             activite_principale == Activite.SOCIETES_GESTION_FONDS_GARANTIE,
-            activite_principale == Activite.ACTIVITES_CROISIERE,
-            activite_principale == Activite.CONCESSIONS_MINIERES,
-            activite_principale == Activite.MEMBRE_GROUPE_FISCAL,
-            activite_principale == Activite.GIE,
-            activite_principale == Activite.SCPR,
-            activite_principale == Activite.SCM,
-            activite_principale == Activite.OBNL,
-            True
             ], [
             societes_gestion_fonds_garantie.calc(nombre_exercices),
-            activites_croisieres.calc(nombre_exercices),
-            concessions_minieres.calc(nombre_exercices),
-            membre_groupe_fiscal.calc(nombre_exercices),
-            gie.calc(nombre_exercices),
-            scpr.calc(nombre_exercices),
-            scm.calc(nombre_exercices),
-            obnl.calc(nombre_exercices),
-            0.0
-            ])
+            ], 0.0)
 
         abattement = where(abattement_taux_est_a_saisir, abattement_saisie, abattement)
         abattement = where(possede_abattement, abattement, 0)
@@ -193,8 +155,21 @@ class is_brut_base(Variable):
 
     def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
         resultat_fiscal = personne('is_resultat_fiscal_benefice_apres_report_deficitaire', period)
-        is_brut_base = arrondi_millier_inferieur(resultat_fiscal)
-        return is_brut_base
+        base = arrondi_millier_inferieur(resultat_fiscal)
+        return base
+
+
+class is_brut_base_imposable(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Base Brut imposable Is Societe"
+
+    def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
+        base = personne('is_brut_base', period)
+        abattement = personne('is_brut_abattement_taux', period)
+        base_imposable = base * (1 - abattement)
+        return base_imposable
 
 
 class is_brut_due(Variable):
@@ -204,9 +179,7 @@ class is_brut_due(Variable):
     label = "Somme Brut Is Societe Due"
 
     def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
-
-        brut_base = personne('is_brut_base', period)
-        abattement = personne('is_brut_abattement_taux', period)
+        brut_base = personne('is_brut_base_imposable', period)
         taux_brut = personne('is_brut_taux', period)
-        is_brut_due = brut_base * (1 - abattement) * taux_brut
+        is_brut_due = brut_base * taux_brut
         return is_brut_due
