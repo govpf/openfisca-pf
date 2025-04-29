0832dd9fd885c3ab50d18ffa50825bdb0861a927
antoineleloarer <antoine.le-loarer@gmail.com>
2025-04-29 17:19:59 +0200

    feat: creances et dettes

---
+++ openfisca_pf/variables/dicp/impot_societe/creances/actif_circulant.py
@@ -0,0 +1,215 @@
+# Import the Entities specifically defined for this tax and benefit system
+from openfisca_pf.base import (
+    Variable,
+    DAY
+    )
+from openfisca_pf.entities import Personne
+
+
+class is_creances_clients_douteux_ou_litigieux_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Clients douteux ou litigieux montant brut (VA)"
+
+
+class is_creances_clients_douteux_ou_litigieux_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Clients douteux ou litigieux à un an au plus (VA)"
+
+
+class is_creances_clients_douteux_ou_litigieux_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Clients douteux ou litigieux à plus d'un an (VA)"
+
+
+class is_creances_autres_creances_clients_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Autres créances clients montant brut (7N)"
+
+
+class is_creances_autres_creances_clients_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Autres créances clients à un an au plus (7N)"
+
+
+class is_creances_autres_creances_clients_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Autres créances clients à plus d'un an (7N)"
+
+class is_creances_personnel_et_comptes_rattaches_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Personnel et comptes rattachés montant brut (7P)"
+
+
+class is_creances_personnel_et_comptes_rattaches_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Personnel et comptes rattachés à un an au plus (7P)"
+
+
+class is_creances_personnel_et_comptes_rattaches_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Personnel et comptes rattachés à plus d'un an (7P)"
+
+
+class is_creances_securite_social_et_autres_organismes_sociaux_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Sécurité sociale et autres organismes sociaux montant brut (7R)"
+
+
+class is_creances_securite_social_et_autres_organismes_sociaux_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Sécurité sociale et autres organismes sociaux à un an au plus (7R)"
+
+
+class is_creances_securite_social_et_autres_organismes_sociaux_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Sécurité sociale et autres organismes sociaux à plus d'un an (7R)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_impots_sur_les_benefices_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques impôts sur les bénéfices montant brut (7S)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_impots_sur_les_benefices_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques impôts sur les bénéfices à un an au plus (7S)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_impots_sur_les_benefices_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques impôts sur les bénéfices à plus d'un an (7S)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_taxe_sur_la_valeur_ajoutee_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques taxe sur la valeur ajoutée montant brut (VB)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_taxe_sur_la_valeur_ajoutee_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques taxe sur la valeur ajoutée à un an au plus (VB)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_taxe_sur_la_valeur_ajoutee_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques taxe sur la valeur ajoutée à plus d'un an (VB)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_autres_impots_taxes_et_versements_assimiles_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques autres impôts taxes et versements assimilés montant brut (7T)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_autres_impots_taxes_et_versements_assimiles_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques autres impôts taxes et versements assimilés à un an au plus (7T)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_autres_impots_taxes_et_versements_assimiles_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques autres impôts taxes et versements assimilés à plus d'un an (7T)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_divers_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques divers montant brut (7U)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_divers_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques divers à un an au plus (7U)"
+
+
+class is_creances_etat_et_autres_collectivites_publiques_divers_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "État et autres collectivités publiques divers à plus d'un an (7U)"
+
+
+class is_creances_groupe_et_associes_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Groupe et associés montant brut (VC)"
+
+
+class is_creances_groupe_et_associes_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Groupe et associés à un an au plus (VC)"
+
+
+class is_creances_groupe_et_associes_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Groupe et associés à plus d'un an (VC)"
+
+
+class is_creances_debiteurs_divers_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Débiteurs divers montant brut (7V)"
+
+
+class is_creances_debiteurs_divers_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Débiteurs divers à un an au plus (7V)"
+
+
+class is_creances_debiteurs_divers_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Débiteurs divers à plus d'un an (7V)"
---
+++ openfisca_pf/variables/dicp/impot_societe/creances/totaux.py
@@ -0,0 +1,81 @@
+# Import the Entities specifically defined for this tax and benefit system
+from openfisca_pf.base import (
+    ArrayLike,
+    DAY,
+    ParameterNode,
+    Period,
+    Population,
+    Variable
+    )
+from openfisca_pf.entities import Personne
+
+
+class is_creances_total_montant_brut(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Total (7X)"
+
+    def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
+        creances_rattaches_a_des_participations = personne('is_creances_creances_rattachees_a_des_participations_montant_brut', period)
+        prets = personne('is_creances_prets_montant_brut', period)
+        autres_immobilisations_financieres = personne('is_creances_autres_immobilisations_financieres_montant_brut', period)
+        clients_douteux_ou_litigieux = personne('is_creances_clients_douteux_ou_litigieux_montant_brut', period)
+        autres_creances_clients = personne('is_creances_autres_creances_clients_montant_brut', period)
+        personnel_et_comptes_rattaches = personne('is_creances_personnel_et_comptes_rattaches_montant_brut', period)
+        securite_social_et_autres_organismes_sociaux = personne('is_creances_securite_social_et_autres_organismes_sociaux_montant_brut', period)
+        impots_benefice = personne('is_creances_etat_et_autres_collectivites_publiques_impots_sur_les_benefices_montant_brut', period)
+        taxe_valeur_ajoutee = personne('is_creances_etat_et_autres_collectivites_publiques_taxe_sur_la_valeur_ajoutee_montant_brut', period)
+        autre_impots_taxes_versement_assimilies = personne('is_creances_etat_et_autres_collectivites_publiques_autres_impots_taxes_et_versements_assimiles_montant_brut', period)
+        divers = personne('is_creances_etat_et_autres_collectivites_publiques_divers_montant_brut', period)
+        groupe_associes = personne('is_creances_groupe_et_associes_montant_brut', period)
+        debiteurs_divers = personne('is_creances_debiteurs_divers_montant_brut', period)
+        return creances_rattaches_a_des_participations + prets + autres_immobilisations_financieres + clients_douteux_ou_litigieux + autres_creances_clients + personnel_et_comptes_rattaches + securite_social_et_autres_organismes_sociaux + impots_benefice + taxe_valeur_ajoutee + autre_impots_taxes_versement_assimilies + divers + groupe_associes + debiteurs_divers
+
+
+class is_creances_total_a_un_an_au_plus(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Total à un an au plus (7X)"
+
+    def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
+        creances_rattaches_a_des_participations = personne('is_creances_creances_rattachees_a_des_participations_a_un_an_au_plus', period)
+        prets = personne('is_creances_prets_a_un_an_au_plus', period)
+        autres_immobilisations_financieres = personne('is_creances_autres_immobilisations_financieres_a_un_an_au_plus', period)
+        clients_douteux_ou_litigieux = personne('is_creances_clients_douteux_ou_litigieux_a_un_an_au_plus', period)
+        autres_creances_clients = personne('is_creances_autres_creances_clients_a_un_an_au_plus', period)
+        personnel_et_comptes_rattaches = personne('is_creances_personnel_et_comptes_rattaches_a_un_an_au_plus', period)
+        securite_social_et_autres_organismes_sociaux = personne('is_creances_securite_social_et_autres_organismes_sociaux_a_un_an_au_plus', period)
+        impots_benefice = personne('is_creances_etat_et_autres_collectivites_publiques_impots_sur_les_benefices_a_un_an_au_plus', period)
+        taxe_valeur_ajoutee = personne('is_creances_etat_et_autres_collectivites_publiques_taxe_sur_la_valeur_ajoutee_a_un_an_au_plus', period)
+        autre_impots_taxes_versement_assimilies = personne('is_creances_etat_et_autres_collectivites_publiques_autres_impots_taxes_et_versements_assimiles_a_un_an_au_plus', period)
+        divers = personne('is_creances_etat_et_autres_collectivites_publiques_divers_a_un_an_au_plus', period)
+        groupe_associes = personne('is_creances_groupe_et_associes_a_un_an_au_plus', period)
+        debiteurs_divers = personne('is_creances_debiteurs_divers_a_un_an_au_plus', period)
+        return creances_rattaches_a_des_participations + prets + autres_immobilisations_financieres + clients_douteux_ou_litigieux + autres_creances_clients + personnel_et_comptes_rattaches + securite_social_et_autres_organismes_sociaux + impots_benefice + taxe_valeur_ajoutee + autre_impots_taxes_versement_assimilies + divers + groupe_associes + debiteurs_divers
+
+
+class is_creances_total_a_plus_d_un_an(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Total à plus d'un an (7X)"
+
+    def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
+        creances_rattaches_a_des_participations = personne('is_creances_creances_rattachees_a_des_participations_a_plus_d_un_an', period)
+        prets = personne('is_creances_prets_a_plus_d_un_an', period)
+        autres_immobilisations_financieres = personne('is_creances_autres_immobilisations_financieres_a_plus_d_un_an', period)
+        clients_douteux_ou_litigieux = personne('is_creances_clients_douteux_ou_litigieux_a_plus_d_un_an', period)
+        autres_creances_clients = personne('is_creances_autres_creances_clients_a_plus_d_un_an', period)
+        personnel_et_comptes_rattaches = personne('is_creances_personnel_et_comptes_rattaches_a_plus_d_un_an', period)
+        securite_social_et_autres_organismes_sociaux = personne('is_creances_securite_social_et_autres_organismes_sociaux_a_plus_d_un_an', period)
+        impots_benefice = personne('is_creances_etat_et_autres_collectivites_publiques_impots_sur_les_benefices_a_plus_d_un_an', period)
+        taxe_valeur_ajoutee = personne('is_creances_etat_et_autres_collectivites_publiques_taxe_sur_la_valeur_ajoutee_a_plus_d_un_an', period)
+        autre_impots_taxes_versement_assimilies = personne('is_creances_etat_et_autres_collectivites_publiques_autres_impots_taxes_et_versements_assimiles_a_plus_d_un_an', period)
+        divers = personne('is_creances_etat_et_autres_collectivites_publiques_divers_a_plus_d_un_an', period)
+        groupe_associes = personne('is_creances_groupe_et_associes_a_plus_d_un_an', period)
+        debiteurs_divers = personne('is_creances_debiteurs_divers_a_plus_d_un_an', period)
+        return creances_rattaches_a_des_participations + prets + autres_immobilisations_financieres + clients_douteux_ou_litigieux + autres_creances_clients + personnel_et_comptes_rattaches + securite_social_et_autres_organismes_sociaux + impots_benefice + taxe_valeur_ajoutee + autre_impots_taxes_versement_assimilies + divers + groupe_associes + debiteurs_divers
+
+
