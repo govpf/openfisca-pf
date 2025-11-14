dc77b74c914223c0e4faabc14590910356f5b206
fabien.le-houedec <fabien.le-houedec@devmachine.fr>
2025-11-14 17:02:35 +0100

    feat: add sisae avantage

---
+++ openfisca_pf/variables/dicp/impot_societe/avantages/sisae.py
@@ -0,0 +1,40 @@
+from openfisca_pf.base import (
+    ArrayLike,
+    DAY,
+    ParameterNode,
+    Period,
+    Population,
+    Variable,
+    )
+from openfisca_pf.entities import Personne
+
+class is_avantage_sisae_montant(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    unit = 'currency-XFP'
+    label = "Montant attestation SISAE en FCFP"
+
+class is_avantage_sisae_taux_exoneration(Variable):
+    value_type = float
+    entity = Personne
+    definition_period = DAY
+    label = "Taux d'exonération"
+
+    def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
+        return parameters(period).dicp.impot_societe.avantages.sisae.taux_exoneration
+    
+class is_avantage_sisae_credit_ouvert(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    unit = 'currency-XFP'
+    label = "Crédit ouvert attestation SISAE en FCFP"
+
+    def formula(personne: Population, period: Period) -> ArrayLike:
+        taux_exoneration = personne('is_avantage_sisae_taux_exoneration', period)
+        montant = personne('is_avantage_sisae_montant', period)
+        return montant * taux_exoneration
+
+
+
