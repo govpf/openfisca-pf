182781b701d267c465e04ea671179929b21f696b
antoineleloarer <antoine.le-loarer@gmail.com>
2025-11-24 17:20:20 +0100

    fix : linter

--- openfisca_pf/variables/dicp/impot_societe/penalites/penalites.py
+++ openfisca_pf/variables/dicp/impot_societe/penalites/penalites.py
@@ -8,9 +8,10 @@ from openfisca_pf.base import (
     Population,
     Variable,
     DAY
-    )
+)
 from openfisca_pf.entities import Personne
 import numpy as np
+
 
 class is_date_de_depot(Variable):
     value_type = date
@@ -32,7 +33,7 @@ class is_nombre_mois_retard(Variable):
         date_cloture = period.date
         date_depot = personne("date_de_depot", period, parameters)[0].astype('M8[M]').astype(object)
         if date_cloture.month < 2:
-           dlim = datetime.date(date_cloture.year, 7, 31)
+            dlim = datetime.date(date_cloture.year, 7, 31)
         else:
             if date_cloture.month < 8:
                 dlim = datetime.date(date_cloture.year + 1, 1, 31)
@@ -40,5 +41,5 @@ class is_nombre_mois_retard(Variable):
                 dlim = datetime.date(date_cloture.year + 1, 7, 31)
         # Si Société → 0 mois, sinon → +1 mois
         delta = np.where(
-          personne('est_societe', period.this_year), 0, 1)
-        return ( 12 - dlim.month + date_depot.month + ((date_depot.year - dlim.year) - 1 ) * 12 ) - delta
+            personne('est_societe', period.this_year), 0, 1)
+        return (12 - dlim.month + date_depot.month + ((date_depot.year - dlim.year) - 1) * 12) - delta
