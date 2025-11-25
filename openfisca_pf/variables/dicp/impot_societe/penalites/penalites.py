7079f7e7c7a6edb4417df37f788b92cabc36f183
AlexisDefranoux <25173038+AlexisDefranoux@users.noreply.github.com>
2025-11-25 10:17:33 -1000

    Update penalites.py

--- openfisca_pf/variables/dicp/impot_societe/penalites/penalites.py
+++ openfisca_pf/variables/dicp/impot_societe/penalites/penalites.py
@@ -33,16 +33,15 @@ class is_nombre_mois_retard(Variable):
         date_cloture = period.date
         date_depot = personne("is_date_de_depot", period, parameters)[0].astype('M8[M]').astype(object)
         if date_cloture.month < 2:
-            dlim = datetime.date(date_cloture.year, 7, 31)
+            date_limite_depot = datetime.date(date_cloture.year, 7, 31)
         else:
             if date_cloture.month < 8:
-                dlim = datetime.date(date_cloture.year + 1, 1, 31)
+                date_limite_depot = datetime.date(date_cloture.year + 1, 1, 31)
             else:
-                dlim = datetime.date(date_cloture.year + 1, 7, 31)
-        # Si Société → 0 mois, sinon → +1 mois
-        delta = np.where(
-            personne('est_societe', period.this_year), 0, 1)
-        nb_mois = (12 - dlim.month + date_depot.month + ((date_depot.year - dlim.year) - 1) * 12) - delta
+                date_limite_depot = datetime.date(date_cloture.year + 1, 7, 31)
+        # Si îles de la Société → 0 mois, sinon → +1 mois
+        tolerance_archipel = np.where(personne('est_societe', period.this_year), 0, 1)
+        nb_mois = (12 - date_limite_depot.month + date_depot.month + ((date_depot.year - date_limite_depot.year) - 1) * 12) - tolerance_archipel
 
         if nb_mois < 0:
             return 0
