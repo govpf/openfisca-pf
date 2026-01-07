2d91bbfe0b80e4169a29bff137e64312962b965f
Jordan FAILLOUX <jordan.failloux@administration.gov.pf>
2026-01-06 15:28:26 -1000

    feat: add new variables for formula

--- openfisca_pf/variables/daf/rch/acte/tpi.py
+++ openfisca_pf/variables/daf/rch/acte/tpi.py
@@ -15,13 +15,20 @@ from openfisca_pf.base import (
 from openfisca_pf.entities import Personne
 from openfisca_pf.enums.rch import (
     NatureActe,
-    TypeAcheteur,
-    TypeBien,
-    TypeDemarche,
-    TypeParente
+    TypeActe,
     )
 from openfisca_pf.functions.currency import arrondi_superieur
+from openfisca_pf.constants.units import BOOLEAN
 import numpy
+
+
+class type_acte(Variable):
+    value_type = Enum
+    possible_values = TypeActe
+    default_value = TypeActe.Transcription
+    entity = Personne
+    definition_period = DAY
+    label = "Type d'acte auprès de la RCH"
 
 
 class nature_acte(Variable):
@@ -33,11 +40,34 @@ class nature_acte(Variable):
     label = "Nature de l'acte"
 
 
+class disposition(Variable):
+    value_type = bool
+    entity = Personne
+    definition_period = DAY
+    label = "Indique si l'acte fait l'objet d'une disposition"
+    unit = BOOLEAN
+
+
+class regime_de_faveur(Variable):
+    value_type = bool
+    entity = Personne
+    definition_period = DAY
+    label = "Indique si l'acte bénéficie d'un régime de faveur"
+    unit = BOOLEAN
+
+
 class montant_total_acte(Variable):
     value_type = int
     entity = Personne
     definition_period = DAY
     label = "Montant total d'un acte"
+
+
+class montant_initial_acte(Variable):
+    value_type = int
+    entity = Personne
+    definition_period = DAY
+    label = "Montant de l'inscription initial d'un acte"
 
 
 class montant_tpi_acte(Variable):
