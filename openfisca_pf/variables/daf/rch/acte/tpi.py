b70517f3752d5c1dbd64826d7ccb4be53208d1ce
Jordan FAILLOUX <jordan.failloux@administration.gov.pf>
2026-04-09 11:46:02 -1000

    feat: update tpi formula for Personne entity

--- openfisca_pf/variables/daf/rch/acte/tpi.py
+++ openfisca_pf/variables/daf/rch/acte/tpi.py
@@ -11,7 +11,7 @@ from openfisca_pf.base import (
     select,
     Variable
     )
-from openfisca_pf.entities import Acte, Personne
+from openfisca_pf.entities import Personne
 from openfisca_pf.enums.rch import (
     DispositionVariables,
     NatureActe,
@@ -26,19 +26,19 @@ class nature_acte(Variable):
     value_type = Enum
     possible_values = NatureActe
     default_value = NatureActe.Vente
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Nature de l'acte"
 
 
 class is_disposition(Variable):
     value_type = bool
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Indique si la nature de l'acte est une disposition"
 
-    def formula(acte: Population, period: Period) -> ArrayLike:
-        nature_acte = acte('nature_acte', period)
+    def formula(personne: Population, period: Period) -> ArrayLike:
+        nature_acte = personne('nature_acte', period)
         return (
             (nature_acte == NatureActe.Rectification)
             | (nature_acte == NatureActe.Renonciation)
@@ -69,7 +69,7 @@ class type_acte(Variable):
     value_type = Enum
     possible_values = TypeActe
     default_value = TypeActe.Transcription
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Type d'acte auprès de la RCH"
 
@@ -121,28 +121,28 @@ class regime_faveur(Variable):
     value_type = Enum
     possible_values = RegimeFaveur
     default_value = RegimeFaveur.Aucun
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Indique un régime de faveur de l'acte"
 
 
 class montant_total_acte(Variable):
     value_type = int
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Montant total d'un acte"
 
 
 class montant_initial_acte(Variable):
     value_type = int
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Montant de l'inscription initial d'un acte"
 
 
 class taux_tpi(Variable):
     value_type = float
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Taux de la taxe de publicité immobilière selon le type d'acte"
 
@@ -237,23 +237,22 @@ class montant_taxe_disposition(Variable):
 
 class montant_tpi_acte(Variable):
     value_type = int
-    entity = Acte
+    entity = Personne
     definition_period = DAY
     label = "Montant de la taxe de publicité immobilière selon la nature de l'acte"
 
-    def formula(acte: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
-        type_acte = acte('type_acte', period)
-        nature_acte = acte('nature_acte', period)
-        is_disposition = acte('is_disposition', period)
-        regime_faveur = acte('regime_faveur', period)
-        montant_total_acte = acte('montant_total_acte', period)
-        montant_initial_acte = acte('montant_initial_acte', period)
+    def formula(personne: Population, period: Period, parameters: ParameterNode) -> ArrayLike:
+        type_acte = personne('type_acte', period)
+        nature_acte = personne('nature_acte', period)
+        is_disposition = personne('is_disposition', period)
+        regime_faveur = personne('regime_faveur', period)
+        montant_total_acte = personne('montant_total_acte', period)
+        montant_initial_acte = personne('montant_initial_acte', period)
+        montant_disposition = personne('montant_taxe_disposition', period)
 
-        taux_tpi = acte('taux_tpi', period) / 100  # conversion pourcentage en décimal
+        taux_tpi = personne('taux_tpi', period) / 100  # conversion pourcentage en décimal
         fixed_default_value = parameters(period).daf.rch.taxe_publicite_immobiliere.acte.fixed.default
 
-        list_montant_disposition = acte.members('montant_taxe_disposition', period)
-        montant_disposition = acte.sum(list_montant_disposition)
         montant_tpi = select(
             [
                 (regime_faveur != RegimeFaveur.Aucun) | (nature_acte == NatureActe.ActeAdministratif),
